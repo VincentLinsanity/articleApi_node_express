5b06a6c63591ee6f59857cbb0756b29d572687ca
Vincent <vincent.lin@langlive.com>
2019-11-21 16:52:43 +0800

    add api users/articles document

--- routes/articles.js
+++ routes/articles.js
@@ -5,14 +5,57 @@ const router = express.Router();
 const middleware = require("../middlewares");
 const articleService = require("../services/articles");
 
+/**
+ * @swagger
+ * definitions:
+ *   articlesCreate:
+ *     description: request successful
+ *     properties:
+ *       code:
+ *         type: number
+ *         example: "201"
+ *       message:
+ *         type: string
+ *         example: "ok"
+ */
+/**
+ * @swagger
+ * /api/articles:
+ *   post:
+ *     tags:
+ *       - articles
+ *     summary: "articles create api"
+ *     description: "建立文章"
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: header
+ *         name: account
+ *         required: true
+ *       - in: header
+ *         name: token
+ *         required: true
+ *       - in: body
+ *         name: subject
+ *         required: true
+ *       - in: body
+ *         name: content
+ *         required: true
+ *     responses:
+ *       201:
+ *         schema:
+ *         type: object
+ *         $ref: '#/definitions/articlesCreate'
+ */
 router.post("/", middleware.authorize, async (req, res) => {
   const { subject = "", content = "" } = req.body;
   if (subject === "" || content === "") {
-    res.status = 422;
+    res.status(422);
     res.send({
       code: 422,
-      message: "incorrect body data",
-      data: []
+      message: "incorrect body data"
     });
     return;
   }
@@ -26,18 +69,46 @@ router.post("/", middleware.authorize, async (req, res) => {
   } catch (error) {
     res.send({
       code: 400,
-      message: error.message,
-      data: []
+      message: error.message
     });
     return;
   }
   res.send({
     code: 201,
-    message: "ok",
-    data: []
+    message: "ok"
   });
 });
 
+/**
+ * @swagger
+ * /api/articles/{id}:
+ *   put:
+ *     tags:
+ *       - articles
+ *     summary: "articles update api"
+ *     description: "更新文章"
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: header
+ *         name: account
+ *         required: true
+ *       - in: header
+ *         name: token
+ *         required: true
+ *       - in: body
+ *         name: subject
+ *         required: false
+ *       - in: body
+ *         name: content
+ *         required: false
+ *     responses:
+ *       201:
+ *         schema:
+ *         type: object
+ */
 router.put("/:id", middleware.authorize, async (req, res) => {
   const { id } = req.params;
   const { subject = "", content = "" } = req.body;
@@ -54,10 +125,34 @@ router.put("/:id", middleware.authorize, async (req, res) => {
     });
     return;
   }
-  res.status = 201;
+  res.status(201);
   res.send({ code: 201, message: "ok", data: [] });
 });
 
+/**
+ * @swagger
+ * /api/articles/{id}:
+ *   delete:
+ *     tags:
+ *       - articles
+ *     summary: "articles deleted api"
+ *     description: "刪除文章"
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: header
+ *         name: account
+ *         required: true
+ *       - in: header
+ *         name: token
+ *         required: true
+ *     responses:
+ *       201:
+ *         schema:
+ *         type: object
+ */
 router.delete("/:id", middleware.authorize, async (req, res) => {
   const { id } = req.params;
   try {
@@ -70,10 +165,48 @@ router.delete("/:id", middleware.authorize, async (req, res) => {
     });
     return;
   }
-  res.status = 201;
+  res.status(201);
   res.send({ code: 201, message: "ok", data: [] });
 });
 
+/**
+ * @swagger
+ * definitions:
+ *   articlesGet:
+ *     description: request successful
+ *     properties:
+ *       code:
+ *         type: number
+ *         example: "200"
+ *       message:
+ *         type: string
+ *         example: "ok"
+ *       data:
+ *         type: []
+ *         example: [{"subject": "test","content": "test", "author": {
+            "account": "12345",
+            "name": "1234"
+        }}]
+ */
+/**
+ * @swagger
+ * /api/articles/{id}:
+ *   get:
+ *     tags:
+ *       - articles
+ *     summary: "articles get api"
+ *     description: "取得文章詳情"
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         schema:
+ *         type: object
+ *         $ref: '#/definitions/articlesGet'
+ */
 router.get("/:id", middleware.authorize, async (req, res) => {
   const { id = "" } = req.params;
   if (id === "") {
@@ -99,6 +232,41 @@ router.get("/:id", middleware.authorize, async (req, res) => {
   res.send({ code: 200, message: "ok", data: result });
 });
 
+/**
+ * @swagger
+ * definitions:
+ *   articlesList:
+ *     description: request successful
+ *     properties:
+ *       code:
+ *         type: number
+ *         example: "200"
+ *       message:
+ *         type: string
+ *         example: "ok"
+ *       data:
+ *         type: []
+ *         example: [{"subject": "test","content": "test", "_id": "5dd6482c8b1aedc1f31d36d5"}]
+ */
+/**
+ * @swagger
+ * /api/articles:
+ *   get:
+ *     tags:
+ *       - articles
+ *     summary: "articles list api"
+ *     description: "取得文章列表"
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *     responses:
+ *       200:
+ *         schema:
+ *         type: object
+ *         $ref: '#/definitions/articlesList'
+ */
 router.get("/", async (req, res) => {
   const { size, page, desc, order } = req.query;
   let data = [];
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,48 @@ const express = require("express");
 const router = express.Router();
 const usersService = require("../services/users");
 
+/**
+ * @swagger
+ * definitions:
+ *   usersLogin:
+ *     description: request successful
+ *     properties:
+ *       code:
+ *         type: number
+ *         example: "201"
+ *       message:
+ *         type: string
+ *         example: "ok"
+ *       data:
+ *         type: string
+ *         example: eyJhbGciOiJIUzI1N
+ *         description: "user token"
+ */
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     tags:
+ *       - users
+ *     summary: "users login api"
+ *     description: "使用者登入"
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: account
+ *         required: true
+ *       - in: body
+ *         name: password
+ *         required: true
+ *     responses:
+ *       201:
+ *         schema:
+ *         type: object
+ *         $ref: '#/definitions/usersLogin'
+ */
 router.post("/login", async (req, res) => {
   const { account, password } = req.body;
   if (!account || !password) {
@@ -19,10 +61,37 @@ router.post("/login", async (req, res) => {
     res.send({ code: 400, message: error.message, data: [] });
     return;
   }
-  res.status = 201;
+  res.status(201);
   res.send({ code: 201, message: "ok", data: result });
 });
 
+/**
+ * @swagger
+ * /api/users/register:
+ *   post:
+ *     tags:
+ *       - users
+ *     summary: "users register api"
+ *     description: "使用者註冊"
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: body
+ *         name: account
+ *         required: true
+ *       - in: body
+ *         name: password
+ *         required: true
+ *       - in: body
+ *         name: name
+ *         required: true
+ *     responses:
+ *       201:
+ *         schema:
+ *         type: object
+ */
 router.post("/register", async (req, res) => {
   const { account, name, password } = req.body;
   if (!account || !name || !password) {
@@ -37,7 +106,7 @@ router.post("/register", async (req, res) => {
     res.send({ code: 400, message: error.message, data: [] });
     return;
   }
-  res.status = 201;
+  res.status(201);
   res.send({ code: 201, message: "ok", data: [] });
 });
 
